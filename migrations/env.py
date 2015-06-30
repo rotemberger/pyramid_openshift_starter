2a30c4a17bb70123de9ad190707bf8a9510d831d
Rotem Berger <rotemberger@gmail.com>
2015-06-30 21:42:44 +0300

    Added alembic migrations.

---
+++ migrations/env.py
@@ -0,0 +1,59 @@
+from __future__ import with_statement
+from alembic import context
+from sqlalchemy import create_engine
+from logging.config import fileConfig
+from st4rt3r.internal.db import Base, postgresql_url
+from pyramid.paster import get_appsettings
+
+
+settings = get_appsettings('production.ini')
+
+config = context.config
+
+fileConfig(config.config_file_name)
+
+target_metadata = Base.metadata
+
+
+def run_migrations_offline():
+    """Run migrations in 'offline' mode.
+
+    This configures the context with just a URL
+    and not an Engine, though an Engine is acceptable
+    here as well.  By skipping the Engine creation
+    we don't even need a DBAPI to be available.
+
+    Calls to context.execute() here emit the given string to the
+    script output.
+
+    """
+    url = postgresql_url(settings)
+    context.configure(
+        url=url, target_metadata=target_metadata, literal_binds=True)
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def run_migrations_online():
+    """Run migrations in 'online' mode.
+
+    In this scenario we need to create an Engine
+    and associate a connection with the context.
+
+    """
+    connectable = create_engine(postgresql_url(settings))
+
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection,
+            target_metadata=target_metadata
+        )
+
+        with context.begin_transaction():
+            context.run_migrations()
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
