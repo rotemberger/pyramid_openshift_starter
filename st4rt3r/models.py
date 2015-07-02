c9017261672900fde73e6b40f2049c19fa7525d0
Rotem Berger <rotemberger@gmail.com>
2015-07-02 22:47:57 +0300

    Removed dummy MyModel.

--- st4rt3r/models.py
+++ st4rt3r/models.py
@@ -1,14 +1,5 @@
-from sqlalchemy import Column, Integer, Text, Index, String
+from sqlalchemy import Column, Integer, String
 from .internal.db import Base
-
-
-class MyModel(Base):
-    __tablename__ = 'models'
-    id = Column(Integer, primary_key=True)
-    name = Column(Text)
-    value = Column(Integer)
-
-Index('my_index', MyModel.name, unique=True, mysql_length=255)
 
 
 class User(Base):
