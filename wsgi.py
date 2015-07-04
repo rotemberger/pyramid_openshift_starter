eedbb69c1b5db9009891fc8ff5def9a15bafa86d
Rotem Berger <rotemberger@gmail.com>
2015-07-04 17:30:15 +0300

    Added to sys.path the venv created by the installer - for local development.

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,15 @@
 import os
-from pyramid.paster import get_app, setup_logging
+import sys
 
 BASE_DIR = os.path.dirname(__file__)
-
 ini_path = os.path.join(BASE_DIR, 'production.ini')
 
-setup_logging(ini_path)
+# add venv to develop on local machine.
+venv_site_packages_dir = os.path.join(BASE_DIR, 'venv/lib/python3.4/site-packages')
+if venv_site_packages_dir not in sys.path:
+    sys.path.append(venv_site_packages_dir)
 
+from pyramid.paster import get_app, setup_logging
+
+setup_logging(ini_path)
 application = get_app(ini_path, 'main')
